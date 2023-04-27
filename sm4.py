cceba5e48e1458c4e5d441e672af88359736b3c7
Android玄机 <38068634+1198287349@users.noreply.github.com>
2023-04-27 19:23:01 +0800

    Update sm4.py

--- sm4.py
+++ sm4.py
@@ -4,7 +4,7 @@ import base64
 
 if __name__ == '__main__':
     key = b'联系QQ1198287349'
-    iv = b'联系QQ1198287349'
+    iv = b'联系QQ1198287349购买'
     value = b'j09XOZYdG2UOeivVZ4ThwSE3v45Jid/6ShlLxSktc0bPOk+GPxPy8ZnFiPhzIYhG8N7yDyF8fne4mIEYuXQhDRg+0kqq3hBRF8IoBh6K1lr2altCECpML75J0c+mGqqqalBRGCF+lA53rcHHh4B4ow=='
     encrypt_value =base64.b64decode(value)
     crypt_sm4 = CryptSM4()
